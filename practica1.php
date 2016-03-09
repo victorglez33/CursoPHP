ac337f2c398b8a8f456dd81f4228a9b808a3b80c
vagrant <vagrant@localhost.localdomain>
2016-03-09 17:46:48 +0000

    agrego variables

---
+++ practica1.php
@@ -0,0 +1,58 @@
+<?php 
+
+// comentario de una sola linea
+
+/*
+ que esta escrito aqui esta comentado
+
+*/
+
+
+print('Hello ');
+
+echo "World \n";
+
+
+$boolean = true; // variable que recibe true or false
+$boolean = false;
+
+$int1 = 12;
+$int2 = -12;
+$int3 = 012; //Base Octal
+$int4 = 0x0f; // Hexadecimal
+// $int = 0b11111111111; //255
+
+$float = 1.234;
+$float = 1.2e3;
+$float = 7e-10;
+
+unset($init1); //elimininar la variable
+
+
+var_dump($float);
+var_dump($int4);
+
+//ARITMETICA
+
+
+$sum = 1+ 1; //suma
+$res = 2 - 1.0; //resta
+$mul = 4 * 3; //multiplicacion
+$div = 2 / 4; //division
+
+
+var_dump($res);
+var_dump($div);
+
+
+$num = 0;
+
+
+
+
+
+
+
+
+
+
